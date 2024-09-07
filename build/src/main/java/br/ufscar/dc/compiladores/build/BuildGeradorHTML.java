3a40dedc14209ba97af00e7b55c5bb9f3c36fb7a
jorgeprj <jlmp2002@gmail.com>
2024-09-07 16:35:44 -0300

    fix: all testes

--- build/src/main/java/br/ufscar/dc/compiladores/build/BuildGeradorHTML.java
+++ build/src/main/java/br/ufscar/dc/compiladores/build/BuildGeradorHTML.java
@@ -16,72 +16,97 @@ public class BuildGeradorHTML extends BUILDBaseVisitor {
     public Void visitMontagem(BUILDParser.MontagemContext ctx) {
         String texto;
         String nome;
-        
-        //inicia a página:
+
+        // Inicia a página:
         saida.append("<html>\n");
         saida.append("<head>\n");
         saida.append("<title>");
         nome = ctx.nome().CADEIA().getText();
         nome = nome.substring(1, nome.length() - 1);
         saida.append("Montagem: " + nome);
-        saida.append("</title> \n");
-        
-        //Para centralizar a imagem:
+        saida.append("</title>\n");
+
+        // Estilos e Fontes:
         saida.append("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n");
+        saida.append("<link rel=\"preconnect\" href=\"https://fonts.googleapis.com\">\n");
+        saida.append("<link rel=\"preconnect\" href=\"https://fonts.gstatic.com\" crossorigin>\n");
+        saida.append("<link href=\"https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap\" rel=\"stylesheet\">\n");
         saida.append("<style>\n");
-        saida.append("img {\n");
-        saida.append("display: block;\n");
-        saida.append("margin-left: auto;\n");
-        saida.append("margin-right: auto;\n");
-        saida.append("}\n");
+        saida.append("body { font-family: 'Inter', sans-serif; margin: 0; padding: 0; background-color: #f4f4f4; color: #333; }\n");
+        saida.append("header { background-color: #ffffff; padding: 8px; box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); }\n");
+        saida.append("nav { display: flex; justify-content: space-between; align-items: center; max-width: 1200px; margin: 0 auto; }\n");
+        saida.append("nav a { text-decoration: none; color: #333; padding: 10px 20px; font-weight: 700; }\n");
+        saida.append("nav a:hover { color: #007BFF; }\n");
+        saida.append("nav img { max-height: 40px; }\n");
+        saida.append("p { font-size: 18px; line-height: 1.6;}\n");
+        saida.append("section { max-width: 1000px; margin: 0 auto; padding: 40px; background-color: #fff; box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); margin-bottom: 20px; margin-top: 20px; }\n");
+        saida.append("h2 { margin: 0; margin-bottom: 10px; }\n");
+        saida.append("ul { list-style: none; padding: 0; }\n");
+        saida.append("li { font-size: 18px; margin-left: 5px; margin-bottom: 5px; }\n");
+        saida.append("img { display: block; margin-left: auto; margin-right: auto; width: 50%; height: auto; }\n");
         saida.append("</style>\n");
-        saida.append("</head> \n");
-        saida.append("<body> \n");
-        saida.append("<header> \n");
-        saida.append("<section> \n");
-        saida.append("<center>\n");
-        
-        //Nome da Montagem:
-        saida.append("<h1> Manual de Instruções: " + nome + "</h1>\n");
-        saida.append("</center>\n");
-        
-        //Adiciona a imagem:
+        saida.append("</head>\n");
+
+        // Corpo do HTML:
+        saida.append("<body>\n");
+
+        // Header com barra de navegação:
+        saida.append("<header>\n");
+        saida.append("<nav>\n");
+        saida.append("<h3>BUILD</h3>\n");  // Coloque o caminho da imagem do logo aqui
+        saida.append("<div>\n");
+        saida.append("<a href=\"#\">Home</a>\n");
+        saida.append("<a href=\"#componentes\">Componentes</a>\n");
+        saida.append("<a href=\"#montagem\">Montagem</a>\n");
+        saida.append("<a href=\"#contato\">Contato</a>\n");
+        saida.append("</div>\n");
+        saida.append("</nav>\n");
+        saida.append("</header>\n");
+
+        // Imagem:
         if (ctx.imagem() != null) {
             texto = ctx.imagem().CADEIA().getText();
             texto = texto.substring(1, texto.length() - 1);
-            saida.append("<img src= " + texto + " alt=" + nome + " style=\"width:400px;height:270px;\" style=\"width:50%;\">\n");
+            saida.append("<section>\n");
+            saida.append("<img src=\"" + texto + "\" alt=\"" + nome + "\" style=\"width:400px;height:270px;\">\n");
+            saida.append("</section>\n");
         }
-        saida.append("<p>Tempo de Montagem: ");
+
+        // Tempo de Montagem:
+        saida.append("<section>\n");
+        saida.append("<h2>Manual de Instruções: " + nome + "</h2>\n");
+        saida.append("<p><strong>Tempo de Montagem:</strong> ");
         for (var tempo : ctx.tempo_mont().tempo()) {
             saida.append(tempo.NUMERO().getText() + " " + tempo.unidade_tempo().getText() + " ");
         }
         saida.append("</p>\n");
+
+        // Parágrafos descritivos:
         for (var par : ctx.paragrafo()) {
             texto = par.CADEIA().getText();
             texto = texto.substring(1, texto.length() - 1);
             saida.append("<p>" + texto + "</p>\n");
         }
         saida.append("</section>\n");
-        saida.append("</header>\n");
-        
-        //Seção de componentes:
-        saida.append("<section id=\"componentes\" >\n");
+
+        // Seção de Componentes:
+        saida.append("<section id=\"componentes\">\n");
         saida.append("<h2>Componentes:</h2>\n");
         visitComponentes(ctx.componentes());
         saida.append("</section>\n");
-        
-        //Seção de montagem:
-        saida.append("<section id=\"montagem\" >\n");
+
+        // Seção de Montagem:
+        saida.append("<section id=\"montagem\">\n");
         saida.append("<h2>Montagem:</h2>\n");
         visitPasso(ctx.passo());
         saida.append("</section>\n");
-        
-        //Termina a página:
+
+        // Finaliza a página:
         saida.append("</body>\n");
-        saida.append("</html>");
+        saida.append("</html>\n");
+
         return null;
     }
-
 
     @Override
     public Void visitLista_pecas(BUILDParser.Lista_pecasContext ctx) {
--- build/src/main/java/br/ufscar/dc/compiladores/build/BuildSemantico.java
+++ build/src/main/java/br/ufscar/dc/compiladores/build/BuildSemantico.java
@@ -14,7 +14,7 @@ public class BuildSemantico extends BUILDBaseVisitor<Void> {
         return super.visitMontagem(ctx);
     }
 
-    //Visitante da lista de pecass: Verifica se a peca já está na tabela e o adiciona:
+    //Visitante da lista de pecas: Verifica se a peca já está na tabela e o adiciona:
     @Override
     public Void visitLista_pecas(BUILDParser.Lista_pecasContext ctx) {
         String nomePec = ctx.PECA().getText();
@@ -29,10 +29,19 @@ public class BuildSemantico extends BUILDBaseVisitor<Void> {
     //Visitante do comando Aparafuse:
     @Override
     public Void visitCmdAparafuse(BUILDParser.CmdAparafuseContext ctx) {
-        String Comp = ctx.PECA().getText();
-        if (!tabela.existe(Comp)) {
-            SemanticoUtils.adicionarErroSemantico(ctx.PECA().getSymbol(), "Componente " + Comp + " não declarado.");
+        // Obtém o nome da peça
+        String pec = ctx.PECA().getText();
+
+        // Verifica se a peça específico está declarado
+        if (!tabela.existe(pec)) {
+            SemanticoUtils.adicionarErroSemantico(ctx.PECA().getSymbol(), "Peça " + pec + " não declarado.");
         }
+
+        // Verifica se a peça "Parafuso" está declarada
+        if (!tabela.existe("Parafuso")) {
+            SemanticoUtils.adicionarErroSemantico(ctx.getStart(), "Parafuso não declarado.");
+        }
+
         return super.visitCmdAparafuse(ctx);
     }
     
@@ -50,12 +59,20 @@ public class BuildSemantico extends BUILDBaseVisitor<Void> {
     //Visitante do comando Cole:
     @Override
     public Void visitCmdCole(BUILDParser.CmdColeContext ctx) {
+        // Verifica se "Cola" está declarado
+        if (!tabela.existe("Cola")) {
+            // Adiciona um erro semântico se "Cola" não estiver declarado
+            SemanticoUtils.adicionarErroSemantico(ctx.getStart(), "Cola não declarada.");
+            tabela.adicionar("Cola", TipoBUILD.PECA);
+        }
+
         for (var pec : ctx.PECA()) {
             String nome_pec = pec.getText();
             if (!tabela.existe(nome_pec)) {
-                SemanticoUtils.adicionarErroSemantico(pec.getSymbol(), "Peça " + nome_pec + " não declarado.");
+                SemanticoUtils.adicionarErroSemantico(pec.getSymbol(), "Peça " + nome_pec + " não declarada.");
             }
         }
+
         return super.visitCmdCole(ctx);
     }
 }
